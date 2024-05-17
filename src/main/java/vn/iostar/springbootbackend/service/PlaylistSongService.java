39c8a39ba696846442e5757c7e9869fcd3151e89
tphuc000 <21110915@student.hcmute.edu.vn>
2024-05-17 12:42:01 +0700

    add: validate playlist name and exist song in playlist

--- src/main/java/vn/iostar/springbootbackend/service/PlaylistSongService.java
+++ src/main/java/vn/iostar/springbootbackend/service/PlaylistSongService.java
@@ -2,11 +2,13 @@ package vn.iostar.springbootbackend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.iostar.springbootbackend.embededId.PlaylistSongId;
 import vn.iostar.springbootbackend.entity.PlaylistSong;
 import vn.iostar.springbootbackend.entity.Song;
 import vn.iostar.springbootbackend.model.SongModel;
 import vn.iostar.springbootbackend.repository.PlaylistSongRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -48,5 +50,16 @@ public class PlaylistSongService {
 
     public void deleteAllById(Iterable<? extends Long> longs) {
         playlistSongRepository.deleteAllById(longs);
+    }
+
+    public void addSongToPlaylist(Long id_playlist, Long id_song) {
+        PlaylistSong entity = new PlaylistSong();
+        entity.setPlaylistSongId(new PlaylistSongId(id_playlist, id_song));
+        entity.setDayAdded(LocalDateTime.now());
+        playlistSongRepository.save(entity);
+    }
+
+    public boolean isSongExistsInPlaylist(Long id_playlist, Long idSong) {
+        return playlistSongRepository.isSongExistsInPlaylist(id_playlist, idSong);
     }
 }
