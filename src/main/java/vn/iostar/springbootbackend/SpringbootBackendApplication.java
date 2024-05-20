e4ca4ade964a81c05060e9ef2b5b5ec4c355738e
THANH\21110 <thanh.161003@gmail.com>
2024-05-21 01:39:19 +0700

    done

--- src/main/java/vn/iostar/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/vn/iostar/springbootbackend/SpringbootBackendApplication.java
@@ -58,7 +58,7 @@ public class SpringbootBackendApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry
 						.addMapping("/**")
-						.allowedOrigins("http://localhost:3000", "http://10.0.2.2:8989", "http://192.168.1.2:8989")
+						.allowedOrigins("http://localhost:3000", "http://10.0.2.2:8989", "http://192.168.1.245:8989")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);;
